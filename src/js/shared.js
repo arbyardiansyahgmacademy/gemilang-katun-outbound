422c67c7fe6417e8af0cc456ec8f3d8829d9a388
arby ardiansyah <arbiardiansyah@gmail.com>
2026-07-27 19:58:21 +0700

    perbaikan contactt

--- src/js/shared.js
+++ src/js/shared.js
@@ -214,7 +214,7 @@ export function renderFooter() {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
                 </div>
-                <a href="https://wa.me/6282211221909" target="_blank" class="text-[#38BDF8] font-bold hover:underline">+62 831-1554-2771 (WA)</a>
+                <a href="https://wa.me/6282211221909" target="_blank" class="text-[#38BDF8] font-bold hover:underline">+62 822-1122-1909 (WA)</a>
               </li>
             </ul>
           </div>
